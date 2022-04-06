aa7938af8d3e00001ce297c5d6798a027ba2c906
northsqrd <41840853+northsqrd@users.noreply.github.com>
2022-04-06 02:46:59 +0200

    feat: more presets!
    Added a few more presets, just to showcase the different categories better.

--- clothing.lua
+++ clothing.lua
@@ -8,15 +8,18 @@ Clothing = {
                 categoryName = "Patrol",
                 presets = {
                     {
-                        name = "Patrol Class A Uniform",
+                        name = "Class A",
                         props = {
-                            {0, 1, 0}, --Hats / Helmets
+                            {0, -1, -1}, --Hats / Helmets
                             {1, -1, -1}, --Glasses
                             {2, -1, -1}, --Misc / Ear Accessories
                             {6, -1, -1}, --Watches
                             {7, -1, -1}, --Bracelets
                         },
                         components = {
+                            --{0, 0, 0}, --Head (Remove unless needed for preset)
+                            {1, 101, 0}, --Mask / Facial Hair (Remove unless needed for preset)
+                            --{2, 18, 0}, --Hair style (Remove unless needed for preset)
                             {3, 4, 0}, --Hands / Upper Body
                             {4, 35, 0}, --Legs / pants
                             {5, 52, 0}, --Hands / Upper Body
@@ -28,13 +31,312 @@ Clothing = {
                             {11, 200, 0}, --Shirt Overlay / Jackets
                         },
                     },
+                    {
+                        name = "Class B",
+                        props = {
+                            {0, -1, -1}, --Hats / Helmets
+                            {1, -1, -1}, --Glasses
+                            {2, -1, -1}, --Misc / Ear Accessories
+                            {6, -1, -1}, --Watches
+                            {7, -1, -1}, --Bracelets
+                        },
+                        components = {
+                            --{0, 0, 0}, --Head (Remove unless needed for preset)
+                            {1, 101, 0}, --Mask / Facial Hair (Remove unless needed for preset)
+                            --{2, 18, 0}, --Hair style (Remove unless needed for preset)
+                            {3, 4, 0}, --Hands / Upper Body
+                            {4, 35, 0}, --Legs / pants
+                            {5, 52, 0}, --Hands / Upper Body
+                            {6, 51, 0}, --Foot
+                            {7, 8, 0}, --Neck / Holsters
+                            {8, 56, 0}, --Shirt / Accessory
+                            {9, 14, 0}, --Body Armor / Accessory 2
+                            {10, 0, 0}, --Badges and Logos
+                            {11, 193, 0}, --Shirt Overlay / Jackets
+                        },
+                    },
+                    {
+                        name = "Class C",
+                        props = {
+                            {0, -1, -1}, --Hats / Helmets
+                            {1, -1, -1}, --Glasses
+                            {2, -1, -1}, --Misc / Ear Accessories
+                            {6, 3, 0}, --Watches
+                            {7, -1, -1}, --Bracelets
+                        },
+                        components = {
+                            --{0, 0, 0}, --Head (Remove unless needed for preset)
+                            {1, 101, 0}, --Mask / Facial Hair (Remove unless needed for preset)
+                            --{2, 18, 0}, --Hair style (Remove unless needed for preset)
+                            {3, 11, 0}, --Hands / Upper Body
+                            {4, 35, 0}, --Legs / pants
+                            {5, 52, 0}, --Hands / Upper Body
+                            {6, 51, 0}, --Foot
+                            {7, 8, 0}, --Neck / Holsters
+                            {8, 56, 0}, --Shirt / Accessory
+                            {9, 13, 0}, --Body Armor / Accessory 2
+                            {10, 0, 0}, --Badges and Logos
+                            {11, 190, 0}, --Shirt Overlay / Jackets
+                        },
+                    },
+                    {
+                        name = "Jacket",
+                        props = {
+                            {0, -1, -1}, --Hats / Helmets
+                            {1, -1, -1}, --Glasses
+                            {2, -1, -1}, --Misc / Ear Accessories
+                            {6, -1, -1}, --Watches
+                            {7, -1, -1}, --Bracelets
+                        },
+                        components = {
+                            --{0, 0, 0}, --Head (Remove unless needed for preset)
+                            {1, 101, 0}, --Mask / Facial Hair (Remove unless needed for preset)
+                            --{2, 18, 0}, --Hair style (Remove unless needed for preset)
+                            {3, 4, 0}, --Hands / Upper Body
+                            {4, 35, 0}, --Legs / pants
+                            {5, 48, 0}, --Hands / Upper Body
+                            {6, 51, 0}, --Foot
+                            {7, 8, 0}, --Neck / Holsters
+                            {8, 56, 0}, --Shirt / Accessory
+                            {9, 28, 0}, --Body Armor / Accessory 2
+                            {10, 0, 0}, --Badges and Logos
+                            {11, 29, 0}, --Shirt Overlay / Jackets
+                        },
+                    },
+                    {
+                        name = "Raincoat",
+                        props = {
+                            {0, -1, -1}, --Hats / Helmets
+                            {1, -1, -1}, --Glasses
+                            {2, -1, -1}, --Misc / Ear Accessories
+                            {6, -1, -1}, --Watches
+                            {7, -1, -1}, --Bracelets
+                        },
+                        components = {
+                            --{0, 0, 0}, --Head (Remove unless needed for preset)
+                            {1, 101, 0}, --Mask / Facial Hair (Remove unless needed for preset)
+                            --{2, 18, 0}, --Hair style (Remove unless needed for preset)
+                            {3, 4, 0}, --Hands / Upper Body
+                            {4, 35, 0}, --Legs / pants
+                            {5, 52, 0}, --Hands / Upper Body
+                            {6, 51, 0}, --Foot
+                            {7, 8, 0}, --Neck / Holsters
+                            {8, 56, 0}, --Shirt / Accessory
+                            {9, 28, 0}, --Body Armor / Accessory 2
+                            {10, 0, 0}, --Badges and Logos
+                            {11, 187, 0}, --Shirt Overlay / Jackets
+                        },
+                    },
                 }
             },
+            {
+                categoryName = "Motor Unit",
+                presets = {
+                    {
+                        name = "Class A",
+                        props = {
+                            {0, 17, 1}, --Hats / Helmets
+                            {1, -1, -1}, --Glasses
+                            {2, -1, -1}, --Misc / Ear Accessories
+                            {6, -1, -1}, --Watches
+                            {7, -1, -1}, --Bracelets
+                        },
+                        components = {
+                            --{0, 0, 0}, --Head (Remove unless needed for preset)
+                            {1, 101, 0}, --Mask / Facial Hair (Remove unless needed for preset)
+                            --{2, 18, 0}, --Hair style (Remove unless needed for preset)
+                            {3, 20, 0}, --Hands / Upper Body
+                            {4, 32, 1}, --Legs / pants
+                            {5, 52, 0}, --Hands / Upper Body
+                            {6, 13, 0}, --Foot
+                            {7, 8, 0}, --Neck / Holsters
+                            {8, 56, 0}, --Shirt / Accessory
+                            {9, 0, 0}, --Body Armor / Accessory 2
+                            {10, 0, 0}, --Badges and Logos
+                            {11, 200, 1}, --Shirt Overlay / Jackets
+                        },
+                    },
+                    {
+                        name = "Class B",
+                        props = {
+                            {0, 17, 1}, --Hats / Helmets
+                            {1, -1, -1}, --Glasses
+                            {2, -1, -1}, --Misc / Ear Accessories
+                            {6, -1, -1}, --Watches
+                            {7, -1, -1}, --Bracelets
+                        },
+                        components = {
+                            --{0, 0, 0}, --Head (Remove unless needed for preset)
+                            {1, 101, 0}, --Mask / Facial Hair (Remove unless needed for preset)
+                            --{2, 18, 0}, --Hair style (Remove unless needed for preset)
+                            {3, 20, 0}, --Hands / Upper Body
+                            {4, 32, 1}, --Legs / pants
+                            {5, 52, 0}, --Hands / Upper Body
+                            {6, 13, 0}, --Foot
+                            {7, 8, 0}, --Neck / Holsters
+                            {8, 56, 0}, --Shirt / Accessory
+                            {9, 0, 0}, --Body Armor / Accessory 2
+                            {10, 0, 0}, --Badges and Logos
+                            {11, 193, 1}, --Shirt Overlay / Jackets
+                        },
+                    },
+                    {
+                        name = "Class C",
+                        props = {
+                            {0, 17, 1}, --Hats / Helmets
+                            {1, -1, -1}, --Glasses
+                            {2, -1, -1}, --Misc / Ear Accessories
+                            {6, -1, -1}, --Watches
+                            {7, -1, -1}, --Bracelets
+                        },
+                        components = {
+                            --{0, 0, 0}, --Head (Remove unless needed for preset)
+                            {1, 101, 0}, --Mask / Facial Hair (Remove unless needed for preset)
+                            --{2, 18, 0}, --Hair style (Remove unless needed for preset)
+                            {3, 26, 0}, --Hands / Upper Body
+                            {4, 32, 1}, --Legs / pants
+                            {5, 52, 0}, --Hands / Upper Body
+                            {6, 13, 0}, --Foot
+                            {7, 8, 0}, --Neck / Holsters
+                            {8, 56, 0}, --Shirt / Accessory
+                            {9, 0, 0}, --Body Armor / Accessory 2
+                            {10, 0, 0}, --Badges and Logos
+                            {11, 190, 1}, --Shirt Overlay / Jackets
+                        },
+                    },
+                }
+            },
+            {
+                categoryName = "Utility",
+                presets = {
+                    {
+                        name = "Class B",
+                        props = {
+                            {0, -1, -1}, --Hats / Helmets
+                            {1, -1, -1}, --Glasses
+                            {2, -1, -1}, --Misc / Ear Accessories
+                            {6, -1, -1}, --Watches
+                            {7, -1, -1}, --Bracelets
+                        },
+                        components = {
+                            --{0, 0, 0}, --Head (Remove unless needed for preset)
+                            {1, 101, 0}, --Mask / Facial Hair (Remove unless needed for preset)
+                            --{2, 18, 0}, --Hair style (Remove unless needed for preset)
+                            {3, 4, 0}, --Hands / Upper Body
+                            {4, 86, 2}, --Legs / pants
+                            {5, 63, 0}, --Hands / Upper Body
+                            {6, 24, 0}, --Foot
+                            {7, 8, 0}, --Neck / Holsters
+                            {8, 94, 0}, --Shirt / Accessory
+                            {9, 37, 0}, --Body Armor / Accessory 2
+                            {10, 0, 0}, --Badges and Logos
+                            {11, 209, 0}, --Shirt Overlay / Jackets
+                        },
+                    },
+                    {
+                        name = "Class C",
+                        props = {
+                            {0, -1, -1}, --Hats / Helmets
+                            {1, -1, -1}, --Glasses
+                            {2, -1, -1}, --Misc / Ear Accessories
+                            {6, -1, -1}, --Watches
+                            {7, -1, -1}, --Bracelets
+                        },
+                        components = {
+                            --{0, 0, 0}, --Head (Remove unless needed for preset)
+                            {1, 101, 0}, --Mask / Facial Hair (Remove unless needed for preset)
+                            --{2, 18, 0}, --Hair style (Remove unless needed for preset)
+                            {3, 11, 0}, --Hands / Upper Body
+                            {4, 86, 2}, --Legs / pants
+                            {5, 63, 0}, --Hands / Upper Body
+                            {6, 24, 0}, --Foot
+                            {7, 8, 0}, --Neck / Holsters
+                            {8, 94, 0}, --Shirt / Accessory
+                            {9, 37, 0}, --Body Armor / Accessory 2
+                            {10, 0, 0}, --Badges and Logos
+                            {11, 212, 0}, --Shirt Overlay / Jackets
+                        },
+                    },
+                    {
+                        name = "Traffic Class B",
+                        props = {
+                            {0, -1, -1}, --Hats / Helmets
+                            {1, -1, -1}, --Glasses
+                            {2, -1, -1}, --Misc / Ear Accessories
+                            {6, -1, -1}, --Watches
+                            {7, -1, -1}, --Bracelets
+                        },
+                        components = {
+                            --{0, 0, 0}, --Head (Remove unless needed for preset)
+                            {1, 101, 0}, --Mask / Facial Hair (Remove unless needed for preset)
+                            --{2, 18, 0}, --Hair style (Remove unless needed for preset)
+                            {3, 4, 0}, --Hands / Upper Body
+                            {4, 86, 2}, --Legs / pants
+                            {5, 63, 0}, --Hands / Upper Body
+                            {6, 24, 0}, --Foot
+                            {7, 8, 0}, --Neck / Holsters
+                            {8, 94, 0}, --Shirt / Accessory
+                            {9, 37, 0}, --Body Armor / Accessory 2
+                            {10, 0, 0}, --Badges and Logos
+                            {11, 209, 15}, --Shirt Overlay / Jackets
+                        },
+                    },
+                    {
+                        name = "Traffic Class C",
+                        props = {
+                            {0, -1, -1}, --Hats / Helmets
+                            {1, -1, -1}, --Glasses
+                            {2, -1, -1}, --Misc / Ear Accessories
+                            {6, -1, -1}, --Watches
+                            {7, -1, -1}, --Bracelets
+                        },
+                        components = {
+                            --{0, 0, 0}, --Head (Remove unless needed for preset)
+                            {1, 101, 0}, --Mask / Facial Hair (Remove unless needed for preset)
+                            --{2, 18, 0}, --Hair style (Remove unless needed for preset)
+                            {3, 11, 0}, --Hands / Upper Body
+                            {4, 86, 2}, --Legs / pants
+                            {5, 63, 0}, --Hands / Upper Body
+                            {6, 24, 0}, --Foot
+                            {7, 8, 0}, --Neck / Holsters
+                            {8, 94, 0}, --Shirt / Accessory
+                            {9, 37, 0}, --Body Armor / Accessory 2
+                            {10, 0, 0}, --Badges and Logos
+                            {11, 212, 15}, --Shirt Overlay / Jackets
+                        },
+                    }
+                }
+            },
+            
             {
                 categoryName = "K9",
                 presets = {
                     {
-                        name = "K9 Class C Uniform",
+                        name = "Class B",
+                        props = {
+                            {0, -1, -1}, --Hats / Helmets
+                            {1, -1, -1}, --Glasses
+                            {2, -1, -1}, --Misc / Ear Accessories
+                            {6, -1, -1}, --Watches
+                            {7, -1, -1}, --Bracelets
+                        },
+                        components = {
+                            --{0, 0, 0}, --Head (Remove unless needed for preset)
+                            {1, 101, 0}, --Mask / Facial Hair (Remove unless needed for preset)
+                            --{2, 18, 0}, --Hair style (Remove unless needed for preset)
+                            {3, 4, 0}, --Hands / Upper Body
+                            {4, 87, 2}, --Legs / pants
+                            {5, 74, 0}, --Hands / Upper Body
+                            {6, 24, 0}, --Foot
+                            {7, 1, 0}, --Neck / Holsters
+                            {8, 37, 0}, --Shirt / Accessory
+                            {9, 14, 0}, --Body Armor / Accessory 2
+                            {10, 12, 0}, --Badges and Logos
+                            {11, 101, 0}, --Shirt Overlay / Jackets
+                        },
+                    },
+                    {
+                        name = "Class C",
                         props = {
                             {0, -1, -1}, --Hats / Helmets
                             {1, -1, -1}, --Glasses
@@ -58,7 +360,7 @@ Clothing = {
                         },
                     }
                 }
-            }
+            },
         },
         components = {
             {
