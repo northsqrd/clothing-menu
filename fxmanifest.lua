1b4b81c3756e8f2d86e0323ab223ee9ec1d89ed4
northsqrd <41840853+northsqrd@users.noreply.github.com>
2022-04-25 13:07:29 +0200

    chore: update fxmanifest.

--- fxmanifest.lua
+++ fxmanifest.lua
@@ -1,6 +1,11 @@
 fx_version "cerulean"
 game "gta5"
 
+author "northsqrd"
+title "Clothing / EUP Menu"
+description "A fully customizable, multi-ped, multi-department clothing / EUP menu for FiveM."
+version "1.0"
+
 file 'index.html'
 ui_page 'index.html'
 
